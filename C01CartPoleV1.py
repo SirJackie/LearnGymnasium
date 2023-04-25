4ee9580d8782bd82c1acb096ad3a8bd6ceb873cb
lyj00912 <3122742760@qq.com>
2023-04-25 18:20:51 +0800

    made a 工程学 cartpole demo

--- C01CartPoleV1.py
+++ C01CartPoleV1.py
@@ -1,14 +1,30 @@
-import gym
+import gymnasium as gym
+from math import pi
 
 env = gym.make('CartPole-v1', render_mode="human")
 env.reset()
 
+myAction = 0
+
 while True:
     env.render()
-    action = env.action_space.sample()
-    observation, reward, terminated, done, info = env.step(action)
+    # action = env.action_space.sample()
+    observation, reward, terminated, done, info = env.step(myAction)
+
+    cartPosition = observation[0]
+    cartVelocity = observation[1]
+    poleAngle = observation[2]
+    poleAngleVelocity = observation[3]
+    print(poleAngle)
+
+    if poleAngle < 0:
+        myAction = 0
+    else:
+        myAction = 1
+
     if terminated:
-        env.reset()
+        # env.reset()
+        break
     if done:
         break
 env.close()
