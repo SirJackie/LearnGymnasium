e9589fd0f559dd50a73675c1c13b359ad0877229
lyj00912 <3122742760@qq.com>
2023-05-13 22:00:21 +0800

    created new version of GymnasiumInstaller.bat

---
+++ GymnasiumInstallerOld.bat
@@ -0,0 +1,7 @@
+set https_proxy=http://10.86.102.3:10000
+set http_proxy=http://10.86.102.3:10000
+
+pip install gymnasium[all]
+
+start https://gymnasium.farama.org/
+jupyter-lab
--- SetDoubanMirror.bat
+++ SetDoubanMirror.bat
@@ -2,8 +2,3 @@ pip config set global.index-url https://pypi.douban.com/simple/
 pip config set install.trusted-host pypi.douban.com
 pip config set install.disable-pip-version-check true
 pip config set install.timeout 6000
-
-pip install gymnasium[classic-control]
-
-start https://gymnasium.farama.org/
-jupyter-lab
